9b503ccca469a26a766e365c67f9fc22775559bc
Burak <48869708+drementer@users.noreply.github.com>
2022-06-01 07:22:26 +0300

    pretier edilip ana illev eklendi

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,7 +2,7 @@
  * @author drementer
  * klncarslanburak@gmail.com
  * @license MIT (https://github.com/drementer/dred/blob/master/LICENSE)
-*/
+ */
 
 /* İmport */
 import {
@@ -45,3 +45,65 @@ got_to_top();
 /* Atamalar */
 const doc = document,
 	body = doc.body;
+
+// Atamalar
+const urun_gorseller = doc.querySelectorAll(".urun__gorsel");
+
+urun_gorseller.forEach((urun_gorsel, i) => {
+	// Atamalar
+	const urun_kart_demo = doc.querySelector(".urun-kart-demo"),
+		urun_kart_kapsayici = doc.querySelector(".urun-kartlar");
+
+	// Her görsele data-urun-id="i + 1" attr'si ekleniyor
+	urun_gorsel.setAttribute("data-urun-id", i + 1);
+
+	// Yeni urun_kart oluşturulup kapsayıcısının içine atılıyor
+	const urun_kart = urun_kart_demo.content.cloneNode(true);
+	urun_kart_kapsayici.appendChild(urun_kart);
+});
+
+// Atamalar
+const urun_kartlar = doc.querySelectorAll(".urun-kart");
+
+urun_kartlar.forEach((urun_kart, i) => {
+	// Her karta data-urun-id="i + 1" attr'si ekleniyor
+	urun_kart.setAttribute("data-urun-id", i + 1);
+
+	// Gorsel bilgileri karta ekleniyor
+	let gorsel = urun_gorseller[i];
+	urun_kart.querySelector(".urun-kart__gorsel").src = gorsel.src;
+	urun_kart.querySelector(".urun-kart__baslik").textContent = gorsel.title;
+
+	urun_kart.addEventListener("click", () => {
+
+		// Atamalar
+		let gorsel_id = urun_kart.dataset.urunId,
+			aktif_gorsel = doc.querySelector(".urun__gorsel.aktif"),
+			gorsel = doc.querySelector(
+				`.urun__gorsel[data-urun-id='${gorsel_id}']`
+			),
+			aktif_kart = doc.querySelector(".urun-kart.aktif");
+		
+		aktif_kart.classList.remove("aktif");
+		urun_kart.classList.add("aktif");
+
+		// Aktif görselden aktif class'ı alınıyor
+		aktif_gorsel.classList.remove("aktif");
+
+		// Artık aktif olmadığı için pasif class'ı ekleniyor
+		aktif_gorsel.classList.add("pasif");
+
+		// Açılacak görselden varsa pasif class'ı alınıp
+		// aktif class'ı veriliyor
+		gorsel.classList.add("aktif");
+		gorsel.classList.contains("pasif") && gorsel.classList.remove("pasif");
+
+		let tema;
+
+		gorsel.dataset.tema ? (tema = gorsel.dataset.tema) : (tema = "#F3EEEE");
+
+		body.style.backgroundColor = tema;
+	});
+});
+
+urun_kartlar[0].classList.add("aktif");
